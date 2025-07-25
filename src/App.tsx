bbb21d162f905796c6463e6fe1dd3a5b9fc1302b
Dexter Williams <47453816+dexterwilliams96@users.noreply.github.com>
2025-07-25 12:15:41 -0500

    Transferring files from temp site

---
+++ src/App.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Skeleton from "@mui/material/Skeleton";
+import Typography from "@mui/material/Typography";
+
+import corp from "./data/corpora.json";
+import pap from "./data/papers.json";
+
+interface Row {
+  id: number;
+  corpora_name: string;
+  paper_name: string;
+  authors: string;
+  date: string;
+  genre: string;
+  language: string;
+  document_types: string;
+  document_count: number;
+  annotation_description: string;
+  annotator_count: number;
+  annotator_type: string;
+  agreement: number;
+  agreement_interpretation: string;
+  accessibility: string;
+  corpora_link: string;
+  paper_link: string;
+}
+
+const columns = [
+  { field: "corpora_name", headerName: "Corpora Name", width: 200 },
+  { field: "paper_name", headerName: "Paper Name", width: 200 },
+  { field: "authors", headerName: "Authors", width: 200 },
+  { field: "date", headerName: "Date", width: 200 },
+  { field: "genre", headerName: "Genre", width: 200 },
+  { field: "language", headerName: "Language", width: 200 },
+  { field: "document_types", headerName: "Document Types", width: 200 },
+  { field: "document_count", headerName: "Document Count", width: 200 },
+  {
+    field: "annotation_description",
+    headerName: "Annotation Description",
+    width: 200,
+  },
+  { field: "annotator_count", headerName: "Annotator Count", width: 200 },
+  { field: "annotator_type", headerName: "Annotator Type", width: 200 },
+  { field: "agreement", headerName: "Agreement", width: 200 },
+  {
+    field: "agreement_interpretation",
+    headerName: "Interpretation",
+    width: 200,
+  },
+  { field: "accessibility", headerName: "Accessibility", width: 200 },
+  { field: "corpora_link", headerName: "Corpora Link", width: 200 },
+  { field: "paper_link", headerName: "Paper Link", width: 200 },
+];
+
+function App() {
+  const [loading] = useState(false);
+  const [rows, setRows] = useState<Row[]>([]);
+
+  useEffect(() => {
+    for (const paper of pap.papers) {
+      console.log(paper);
+      for (const annotation of paper.annotations) {
+        const corpora = corp.corpora.find(
+          (i) => i.corpora_id === annotation.corpora_id,
+        );
+        if (corpora) {
+          setRows([
+            ...rows,
+            {
+              id: paper.paper_id,
+              corpora_name: corpora.corpora_name,
+              paper_name: paper.paper_name,
+              authors: paper.authors,
+              date: paper.date,
+              genre: corpora.genre,
+              language: corpora.language,
+              document_types: corpora.document_types,
+              document_count: corpora.document_count,
+              annotation_description: annotation.annotation_description,
+              annotator_count: annotation.annotator_count,
+              annotator_type: annotation.annotator_type,
+              agreement: annotation.agreement,
+              agreement_interpretation: annotation.agreement_interpretation,
+              accessibility: annotation.accessibility,
+              corpora_link: annotation.corpora_link,
+              paper_link: paper.paper_link,
+            },
+          ]);
+        }
+      }
+    }
+  }, []);
+
+  return (
+    <Box style={{ height: 300, width: "100%" }}>
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <Card variant="outlined">
+          <CardContent>
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14 }}
+            >
+              Grouped by Corpora
+            </Typography>
+            <DataGrid rows={rows} columns={columns} />
+          </CardContent>
+        </Card>
+      )}
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import App from "./App.tsx";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+);
