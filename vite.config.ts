d54db849f22f8e905a144c7e00129d3d88cd6379
Dexter Williams <47453816+dexterwilliams96@users.noreply.github.com>
2025-12-13 21:25:46 -0600

    Updated base in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/arg-mining-corpora/',
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/x-data-grid'],
