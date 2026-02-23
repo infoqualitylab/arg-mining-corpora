face16daf4b0a7d615524a88ddfeccb7a38d8358
dexterwilliams96 <dexterwilliams4355@gmail.com>
2026-02-22 18:14:18 -0600

    minor tweak

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/arg-mining-corpora-4870/',
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/x-data-grid'],
