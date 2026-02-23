295d44d429a268e058eca4c86649364696c9f6c1
dexterwilliams96 <dexterwilliams4355@gmail.com>
2026-02-22 18:05:20 -0600

    anon changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/arg-mining-corpora/',
+  base: 'https://anonymous.4open.science/w/arg-mining-corpora-4780/',
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/x-data-grid'],
